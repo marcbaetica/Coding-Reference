381635d3119746a98aac5febb18054f9ddb7ad9c
abo3mar <a_gadeer@hotmail.com>
2015-07-17 18:05:44 -0700

    added cli example

---
+++ Java/args_program.java
@@ -0,0 +1,18 @@
+public class args_program {
+
+    public static void main (String[] arg) {
+
+        String string = new String("Hello World!/n");
+        System.out.println(string);
+
+        System.out.println(arg[0]);
+        System.out.println(arg[2]);
+
+
+    }
+
+
+
+
+
+}
---
+++ Java/write.java
@@ -0,0 +1,12 @@
+public class write {
+
+
+public static void main (String[] arg) {
+
+    String string = new String();
+    string = "Hello world!";
+    System.out.println(string);
+
+
+}
+}
