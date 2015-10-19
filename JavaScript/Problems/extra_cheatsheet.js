b91ccebda80882065e6a132b81162fdb435502c4
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-10-18 23:45:29 -0400

    added regular expressions

--- JavaScript/Problems/extra_cheatsheet.js
+++ JavaScript/Problems/extra_cheatsheet.js
@@ -57,3 +57,28 @@ var random_N = Math.random();
 console.log(random_N);	//random number
 console.log(Math.floor(random_N*10));	//random number 0-10 rounded down to the nearest integer
 console.log(Math.floor((max-min+1) * random_N) + min);	//random number withing range ... +1 so it doesnt exclude max
+
+
+
+//Sift through Text with REGULAR EXPRESSIONS
+/*Regular expressions are used to find certain words or patterns inside of strings.
+For example, if we wanted to find the word the in the string The dog chased the cat, we could use the following regular expression: /the/gi
+
+the is the pattern we want to match.
+g means that we want to search the entire string for this pattern instead of just the first match.
+i means that we want to ignore the case (uppercase or lowercase) when searching for the pattern.
+
+Regular expressions are written by surrounding the pattern with / symbols.*/
+
+var test = (function() {
+  var testString = "Ada Lovelace and Charles Babbage designed the first computer and the software that would have run on it.";
+
+  var expression = /and/gi;
+  var digit = /\d+/g;		//For finding numbers within a string this is often written as /\d+/g, where the + following the digit selector allows this regular expression to match multi-digit numbers.
+  var whitespaces = /\s+/g;	//The whitespace characters are " " (space), \r (carriage return), \n (newline), \t (tab), and \f (form feed).
+  var expression = /\S/g;	//You can invert any match by using the uppercase version of the selector \s versus \S for example.
+
+  return testString.match(expression).length;
+})();
+
+(function(){console.log(test);})();
