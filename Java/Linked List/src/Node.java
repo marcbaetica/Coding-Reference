89778ddd5f7da6a3b09fbbf3f7df58f229da3e6a
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-10-28 23:58:27 -0400

    updated README.md

--- Java/Linked List/src/Node.java
+++ Java/Linked List/src/Node.java
@@ -8,27 +8,27 @@ public class Node {
 	
 	//constructor
 	public Node(int data) {
-		this.data = data;
+		this.data = data;	//construct the node with its data as the data we insert
 	}
 	
 	
 	//methods for returning the values
 	public int getData() {
-		return this.data;
+		return this.data;	//return the node's data
 	}
 	public Node getNextNode() {
-		return this.nextNode;
+		return this.nextNode;	//return the next node
 	}
 	
 	//methods to set the values
 	public void setData(int data) {
-		this.data = data;
+		this.data = data;	//our node's data gets the required data
 	}
 	public void setNextNode (Node nextNode)	{
-		this.nextNode = nextNode;
+		this.nextNode = nextNode;	//our node's next node gets the upcoming node
 	}
 	
-	//override the toString method and return the data of the current node
+	//override the toString method from the official documenation and return the data of the current node
 	@Override
 	public String toString () {
 		return "Data " + this.data;
